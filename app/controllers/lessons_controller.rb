a501d2ff52f9f84cfc6d3efabb2531b7f51c5fda
Erika <ejderas2@gmail.com>
2018-10-05 01:04:41 +0000

    Payment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,7 +8,7 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user.enrolled_in?(@course)
+    if ! current_user.enrolled_in?(current_lesson.section.course)
       redirect_to course_path(current_lesson.section.course), :alert => "You must be enrolled!"
     end
   end
